583641ac98a6ae0854d0484467b6cd7e9e4f1d54
topbest0123-dot <topbest0123@gmail.com>
2025-10-05 18:13:41 +0100

    Update page.jsx

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -2,9 +2,6 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'force-no-store';
 'use client'
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-export const fetchCache = 'force-no-store';
 import { useState } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 
