a778a3a2cd1df30d709c828be7691c879661e325
topbest0123-dot <topbest0123@gmail.com>
2025-10-05 17:43:46 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
+export const fetchCache = 'force-no-store';
 'use client'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
